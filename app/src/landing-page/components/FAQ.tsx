bd47bbd5ee9dc496699986a80ad61c19b5e812dd
GuillaumeChev71 <guillaumechevallier71@gmail.com>
2025-01-17 22:19:41 +0100

    refactor FAQ component to use details and summary elements for improved accessibility and user experience

--- app/src/landing-page/components/FAQ.tsx
+++ app/src/landing-page/components/FAQ.tsx
@@ -13,19 +13,29 @@ export default function FAQ({ faqs }: { faqs: FAQ[] }) {
       </h2>
       <dl className='mt-10 space-y-8 divide-y divide-gray-900/10'>
         {faqs.map((faq) => (
-          <div key={faq.id} className='pt-8 lg:grid lg:grid-cols-12 lg:gap-8'>
-            <dt className='text-base font-semibold leading-7 text-gray-900 lg:col-span-5 dark:text-white'>
-              {faq.question}
-            </dt>
-            <dd className='flex items-center justify-start gap-2 mt-4 lg:col-span-7 lg:mt-0'>
-              <p className='text-base leading-7 text-gray-600 dark:text-white'>{faq.answer}</p>
-              {faq.href && (
-                <a href={faq.href} className='text-base leading-7 text-yellow-500 hover:text-yellow-600'>
-                  Learn more →
-                </a>
-              )}
-            </dd>
-          </div>
+          <details className="group [&_summary::-webkit-details-marker]:hidden">
+          <summary
+            className="flex cursor-pointer items-center justify-between gap-1.5 rounded-lg bg-gray-50 p-4 text-gray-900"
+          >
+            <h2 className="font-medium">Lorem ipsum dolor sit amet consectetur adipisicing?</h2>
+      
+            <svg
+              className="size-5 shrink-0 transition duration-300 group-open:-rotate-180"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
+            </svg>
+          </summary>
+      
+          <p className="mt-4 px-4 leading-relaxed text-gray-700">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis molestias culpa in,
+            recusandae laboriosam neque aliquid libero nesciunt voluptate dicta quo officiis explicabo
+            consequuntur distinctio corporis earum similique!
+          </p>
+        </details>
         ))}
       </dl>
     </div>
